37c7edb84dceadfb7a691a42a2de5dd37adf613f
shinsuho1 <suhoshin1816@gmail.com>
2023-09-25 23:15:10 +0900

    support white-space 설정

--- script/sub/support.js
+++ script/sub/support.js
@@ -1,8 +1,6 @@
 const support_menus = document.querySelectorAll(".menu>li");
 const support_articles = document.querySelectorAll(".inner>article");
 
-
-
 support_menus.forEach((el,index)=>{
     el.addEventListener("click",()=>{
         for(let i = 0 ; i<support_articles.length ; i++){
@@ -22,9 +20,11 @@ support_title.forEach((el,index)=>{
     el.addEventListener("click",()=>{
         if(support_text[index].classList.contains("on")){
             support_text[index].classList.remove("on");
+            support_title[index].classList.remove("on");
         }
         else{
             support_text[index].classList.add("on");
+            support_title[index].classList.add("on");
         }
     });
 });
