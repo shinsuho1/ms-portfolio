e74357c2f2b5265b74c68b18af8f4d64c57a5097
shinsuho1 <suhoshin1816@gmail.com>
2023-09-27 16:37:38 +0900

    서포트

--- script/sub/support.js
+++ script/sub/support.js
@@ -1,41 +1,29 @@
 const support_menus = document.querySelectorAll(".menu>li");
 const support_articles = document.querySelectorAll(".inner>article");
 
-support_menus.forEach((el,index)=>{
-    el.addEventListener("click",()=>{
-        for(let i = 0 ; i<support_articles.length ; i++){
+support_menus.forEach((el, index) => {
+    el.addEventListener("click", () => {
+        for (let i = 0; i < support_articles.length; i++) {
             support_articles[i].classList.remove("on");
             support_menus[i].classList.remove("on");
         }
         support_menus[index].classList.add("on");
         support_articles[index].classList.add("on");
-
     });
 });
 
+let support_title_box = document.querySelectorAll(".title");
 let support_title = document.querySelectorAll(".title h2");
 let support_text = document.querySelectorAll(".text");
 
-support_title.forEach((el,index)=>{
-    el.addEventListener("click",()=>{
-        if(support_text[index].classList.contains("on")){
+support_title_box.forEach((el, index) => {
+    el.addEventListener("click", () => {
+        if (support_text[index].classList.contains("on")) {
             support_text[index].classList.remove("on");
             support_title[index].classList.remove("on");
-        }
-        else{
+        } else {
             support_text[index].classList.add("on");
             support_title[index].classList.add("on");
         }
     });
 });
-
-
-
-
-
-
-
-
-
-
-
