ac697805b5ef619a30fbd05cc7c5b7943d959f25
shinsuho1 <suhoshin1816@gmail.com>
2023-10-09 00:52:04 +0900

    슬라이더 anime.js사용 X

--- script/slider.js
+++ script/slider.js
@@ -4,12 +4,9 @@ const prev = document.querySelector(".prev");
 const next = document.querySelector(".next");
 let enableClick = true;
 
-
-
 const icons = document.querySelectorAll(".icons>a");
 const icons_arr = Array.from(icons);
 const icon_p = document.querySelectorAll(".icons>p");
-
 
 init();
 
@@ -21,25 +18,25 @@ next.addEventListener("click", (e) => {
         nextSlide_title();
         enableClick = false;
     }
-    
 });
 
 prev.addEventListener("click", (e) => {
     e.preventDefault();
-    
+
     if (enableClick) {
         prevSlide();
         prevSlide_title();
-        enableClick = false
+        enableClick = false;
     }
 });
-icons_arr.map((el,index)=>{
-    icons[index].addEventListener("click",(e)=>{
+
+icons_arr.map((el, index) => {
+    icons[index].addEventListener("click", (e) => {
         e.preventDefault();
         const site = icon_p[index].innerHTML;
-        if(confirm(`${site}`)){
+        if (confirm(`${site}`)) {
             const link = icons_arr[index].getAttribute("href");
-            location.href=link;
+            location.href = link;
         }
     });
 });
@@ -53,59 +50,65 @@ function init() {
 }
 
 function nextSlide() {
-    new Anim(slider, {
-        prop: "left",
-        value: "-200%",
-        duration: 1000,
-        callback: () => {
-            slider.style.left = "-100%";
+    slider.style.transition = "margin-left 1s";
+    slider.style.marginLeft = "-100%";
+
+    slider.addEventListener(
+        "transitionend",
+        () => {
             slider.append(slider.firstElementChild);
+            slider.style.transition = "none";
+            slider.style.marginLeft = "0%";
             enableClick = true;
-        }
-    });
+        },
+        { once: true }
+    );
 }
 
 function nextSlide_title() {
-    new Anim(title, {
-        prop: "left",
-        value: "-200%",
-        duration: 1000,
-        callback: () => {
-            title.style.left = "-100%";
+    title.style.transition = "margin-left 1s";
+    title.style.marginLeft = "-100%";
+
+    title.addEventListener(
+        "transitionend",
+        () => {
             title.append(title.firstElementChild);
+            title.style.transition = "none";
+            title.style.marginLeft = "0%";
             enableClick = true;
-        }
-    });
+        },
+        { once: true }
+    );
 }
 
 function prevSlide() {
-    new Anim(slider, {
-        prop: "left",
-        value: "0%",
-        duration: 1000,
-        callback: () => {
-            slider.style.left = "-100%";
+    slider.style.transition = "margin-left 1s";
+    slider.style.marginLeft = "100%";
+
+    slider.addEventListener(
+        "transitionend",
+        () => {
             slider.prepend(slider.lastElementChild);
+            slider.style.transition = "none";
+            slider.style.marginLeft = "0%";
             enableClick = true;
-        }
-    }); 
+        },
+        { once: true }
+    );
 }
 
 function prevSlide_title() {
-    new Anim(title, {
-        prop: "left",
-        value: "0%",
-        duration: 1000,
-        callback: () => {
-            title.style.left = "-100%";
+    title.style.transition = "margin-left 1s";
+    title.style.marginLeft = "100%";
+
+    title.addEventListener(
+        "transitionend",
+        () => {
             title.prepend(title.lastElementChild);
+            title.style.transition = "none";
+            title.style.marginLeft = "0%";
             enableClick = true;
-        }
-    }); 
+        },
+        { once: true }
+    );
 }
-
-
-const articles = document.querySelectorAll("first>.inner>article");
-const aside = document.querySelector("aside");
-
-
